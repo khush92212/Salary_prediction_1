12d327ad45cc7e35a3eb0fcb6949eeedea341982
khush92212 <aaltufaltu5125@gmail.com>
2026-02-16 10:07:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,31 +10,35 @@ import streamlit as st
 import pandas as pd
 import joblib
 
+# Load model and encoders
 model = joblib.load("salary_prediction_model.pkl")
 encoder = joblib.load("label_encoder.pkl")
 
-st.title("Salary Predictora app")
-age = st.number_input("Age", 18, 60)
+st.title("Salary Predictor App")
+
+age = st.number_input("Age", min_value=18, max_value=60)
 gender = st.selectbox("Gender", encoder["Gender"].classes_)
 education = st.selectbox("Education Level", encoder["Education Level"].classes_)
 job_title = st.selectbox("Job Title", encoder["Job Title"].classes_)
-years_of_exp = st.number_input("Years of Experience", 0, 40)
-
-df = pd.DataFrame({
-    "Age" : [age],
-    "Gender" : [gender],
-    "Education Level" : [education],
-    "Job Title" : [job_title],
-    "Years of Experience" : [years_of_exp]
-})
+years_of_exp = st.number_input("Years of Experience", min_value=0, max_value=40)
 
 if st.button("Predict"):
 
+    df = pd.DataFrame({
+        "Age": [age],
+        "Gender": [gender],
+        "Education Level": [education],
+        "Job Title": [job_title],
+        "Years of Experience": [years_of_exp]
+    })
+
+    # Encode categorical columns
     for col in encoder:
         df[col] = encoder[col].transform(df[col])
 
+    # Reorder columns to match training
     df = df[model.feature_names_in_]
 
-prediction = model.predict(df)
+    prediction = model.predict(df)
 
-st.success(f"Predicted Salary: {prediction[0]:,.2f}")
+    st.success(f"Predicted Salary: {prediction[0]:,.2f}")
